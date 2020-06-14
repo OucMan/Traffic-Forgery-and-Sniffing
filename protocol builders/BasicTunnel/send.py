2b3861521637ed426a7d361d618e0e4fcd0473cb
BigBoy <1668161520@qq.com>
2020-06-14 12:42:14 +0800

    Create send.py

---
+++ protocol builders/BasicTunnel/send.py
@@ -0,0 +1,14 @@
+from scapy.all import *
+from MyTunnelHeader import MyTunnel
+
+def main():
+    iface = 'eth0'
+    dst_id = '2'
+    print('sending on interface %s to %s' % (iface, dst_id))
+    pkt = Ether(src=get_if_hwaddr(iface), dst='ff:ff:ff:ff:ff:ff')
+    pkt = pkt / MyTunnel(dst_id=dst_id) / IP(dst='127.0.0.1') / TCP(dport=1234, sport=random.randint(49152, 65535)) / 'hello'
+    pkt.show2()
+    sendp(pkt, iface=iface, verbose=False)
+
+if __name__ == '__main__':
+    main()
